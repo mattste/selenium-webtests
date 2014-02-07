cdbeca32044e98099cf2bc4f4e5f2f90f7dd955e
Jiří Kuchta <jiri.kuchta@live.com>
2014-02-07 09:17:20 +0100

    platform dependent post-instalation script

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
+import platform
 from distutils.core import setup
+from distutils.command.build_py import build_py as _build_py
+
+script = "scripts/runwebtests"
+if platform.system() == "Windows":
+    script = "scripts/runwebtests.cmd"
 
 setup(
-    name = 'py-selenium-skeleton',
-    packages = ['py-selenium-skeleton'],
+    name = 'selenium-webtests',
+    packages = ['selenium-webtests'],
     version = '0.1',
     description = '',
     author = 'Jiri Kuchta',
     author_email = 'jiri.kuchta@live.com',
-    url = 'https://github.com/jirikuchta/py-selenium-skeleton',
-    keywords = ['testing', 'logging', 'example'],
-    classifiers = [],
+    url = 'https://github.com/jirikuchta/selenium-webtests',
+    keywords = ['testing', 'selenium'],
     install_requires=[
         'selenium>=2.38.4',
         'browsermob-proxy>=0.6.0',
     ],
-    scripts=['scripts/huml'],
+    scripts=[script],
 )
