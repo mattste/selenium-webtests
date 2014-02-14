8ecf15a151268617f0c073ace438452c2dfc01fb
Jiri Kuchta <jiri.kuchta@live.com>
2014-02-14 14:38:45 +0100

    kill proxy on user interrupt

--- seleniumwebtests/__main__.py
+++ seleniumwebtests/__main__.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import sys
+import signal
 from runner import runner
+
+def signal_handler(signal, frame):
+    runner.proxy_server.close()
+    sys.exit(0)
 
 def main():
     runner.run()
-    
+
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
     main()
+
