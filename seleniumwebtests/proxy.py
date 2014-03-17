6e51c2ecb2f9c3dd455f3298de649c02fe8c29fc
Jiri Kuchta <jiri.kuchta@live.com>
2014-03-17 14:45:08 +0100

    methods for searching in proxy results refactored

--- seleniumwebtests/proxy.py
+++ seleniumwebtests/proxy.py
@@ -23,52 +23,86 @@ class Proxy(browsermobproxy.Client):
             options = {"captureHeaders": True, "captureContent": True}
         super(Proxy, self).new_har(name, options)
 
-    def find_in_har(self, url, criterions={}):
+    def test_match(self, url, criterions=[]):
         """
-        This will get the captured HAR and try to find a result matching given criterions.
-        Returns True if a match has been found. False otherwise.
+        Returns True if a request with given url and matching given criterions has been captured.
 
-        :param url: URL or substring of URL of the request we are searching for
-        :param criterions: A dictionary of criterions the result has to match. \
-                Keys in the dictionary represent key names in the HAR theirs value we want \
-                to test. Using dots in the key name it is possible to access data stored \
-                depper in the HAR. Example: \
-                criterions={ \
-                    "response.status": 200, \
-                    "response.headers": { \
-                        "name": "Connection", "value": "closed"} \
-                    } \
-        See examples/har.json to see structure of HAR result.
+        :param url: Regular expression pattern the requst URL has to match
+        :param criterions: List of criterions dicionaries the request has to match. Each dictionary has two items:
+            "key" - dot separated path to the value in the HAR JSON we want to test. , i.e. "response.status", "request.headers" etc.
+                See the examples/har.json to see how one single request is stored in the HAR format.
+            "value" - desired value. Can be string, integer or dict.
         """
-        har = self.har
-        for entry in har["log"]["entries"]:
-            if url in entry["request"]["url"]:
-                for key in criterions:
-                    # get the node in the JSON in which we will be searching
-                    parent = entry
-                    keys = key.split(".")
-                    for k in keys:
-                        try:
-                            parent = parent[k]
-                        except:
-                            raise Exception("key \"" + key + "\" was not found in HAR file")
+        return len(self.get_matches(url, criterions)) != 0
 
-                    # if the parent is a list, search through all items
-                    if isinstance(parent, list):
-                        found = False
-                        for item in parent:
-                            unicriterions = { k.decode('utf8'): v.decode('utf8') for k, v in criterions[key].items() }
-                            uniitems = { k.decode('utf8'): v.decode('utf8') for k, v in item.items() }
-                            if unicriterions == uniitems:
-                                found = True
+    def get_matches(self, url, criterions=[]):
+        """
+        Returns list of matching requests.
 
-                        if not found:
-                            break
-
-                    # if it is integer or string, simply check if it is match the desired value
-                    if isinstance(parent, (int, str)):
-                        if parent != criterions[key]:
-                            break
+        :param url: Regular expression pattern the requst URL has to match
+        :param criterions: List of criterions dicionaries the request has to match. Each dictionary has two items:
+            "key" - dot separated path to the value in the HAR JSON we want to test. , i.e. "response.status", "request.headers" etc.
+                See the examples/har.json to see how one single request is stored in the HAR format.
+            "value" - desired value. Can be string, integer or dict.
+        """
+        url_matches = self._filter_entries_by_url(url)
+        if len(criterions):
+            matches = []
+            criterions_length = len(criterions)-1
+            for entry in url_matches:
+                for i, criterion in enumerate(criterions):
+                    if not self._test_entry_to_criterion_match(entry, criterion):
+                        break
                 else:
+                    if i == criterions_length:
+                        # all criterions match -> passed
+                        matches.append(entry)
+                    continue
+                break
+        else:
+            return url_matches
+        return matches
+
+    def _filter_entries_by_url(self, url):
+        """
+        Filters all captured requests by passed URL
+
+        :param url: Regular expression pattern the requst URL has to match
+        """
+        pattern = re.compile(url)
+        matches = []
+        for entry in self.har["log"]["entries"]:
+            if pattern.search(entry["request"]["url"]):
+                matches.append(entry)
+        return matches
+
+    def _test_entry_to_criterion_match(self, entry, criterion):
+        """
+        Tests single entry in HAR file whether it match given single criterion
+
+        :param entry: Request entry from HAR JSON
+        :param criterion:
+        """
+        parent = self._get_parent_node(entry, criterion["key"])
+        parent_type = type(parent)
+        if parent_type == list:
+            unicriterions = { k.decode('utf8', "replace"): v.decode('utf8', "replace") for k, v in criterion["value"].items() }
+            for item in parent:
+                if set(unicriterions.items()) <= set({ k: v for k, v in item.items() }.items()):
                     return True
-        return False
+            return False
+        if parent_type == str or int:
+            return str(parent) == str(criterion["value"])
+
+    def _get_parent_node(self, entry, key):
+        """
+        Finds item in single request entry matching given criterion key
+        """
+        parent = entry
+        keys = key.split(".")
+        for k in keys:
+            try:
+                parent = parent[k]
+            except:
+                raise Exception("key \"" + key + "\" was not found in HAR file")
+        return parent
